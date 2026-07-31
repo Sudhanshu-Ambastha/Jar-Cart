2c15f9dd15fae377d1680165015c5f176ad3d8ee
Sudhanshu Ambastha <135802131+Sudhanshu-Ambastha@users.noreply.github.com>
2026-07-31 20:47:32 +0530

    ⬆️update

--- jc/tools/chocolateyinstall.ps1
+++ jc/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName = 'jar-cart'
-$url = 'https://github.com/Sudhanshu-Ambastha/jar-cart/releases/download/v0.5.1/jar-cart-x86_64-windows.zip'
-$checksum = '12764C388ACDBA097E1E3EDFB15D181019AED90E250614C05807325B0D2D71ED'
+$url = 'https://github.com/Sudhanshu-Ambastha/jar-cart/releases/download/v6.0/jar-cart-x86_64-windows.zip'
+$checksum = '1dfe41127028dfef2713fdaab694134fcaf1d64dc333ed42eb328a5a17ea849c'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -PackageName $packageName `
-                             -Url $url `
-                             -UnzipLocation $toolsDir `
-                             -Checksum $checksum `
-                             -ChecksumType 'sha256'
+                            -Url $url `
+                            -UnzipLocation $toolsDir `
+                            -Checksum $checksum `
+                            -ChecksumType 'sha256'
 
 Install-BinFile -Name 'jar-cart' -Path (Join-Path $toolsDir 'jar-cart.exe')
 Install-BinFile -Name 'jc' -Path (Join-Path $toolsDir 'jar-cart.exe')
